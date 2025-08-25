2aa4fa68757e3a05c47a2baf849bfd67b0e8e93d
vrodin <mattywut@gmail.com>
2025-08-25 17:47:37 +0300

    Font drawing optimization

--- menu.c
+++ menu.c
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include "font.h"
 
-#define CART_BUS (*(unsigned char *)0x8000)
 #define WORDS_PER_LINE (160 / 8 + 4)
 
 #define CHAR_ON_LINE 18
@@ -26,29 +25,7 @@ static void clear_screen(void)
   memset(SV_VIDEO, 0, 0x2000);
 }
 
-/* Necessary conversion to have 2 bits per pixel with darkest hue */
-/* Remark: The Supervision uses 2 bits per pixel, and bits are mapped into pixels in reversed order */
-static unsigned int __fastcall__ double_bits(unsigned char)
-{
-  __asm__("stz ptr2");
-  __asm__("stz ptr2+1");
-  __asm__("ldy #$08");
-L1:
-  __asm__("lsr a");
-  __asm__("php");
-  __asm__("ror ptr2");
-  __asm__("ror ptr2+1");
-  __asm__("plp");
-  __asm__("ror ptr2");
-  __asm__("ror ptr2+1");
-  __asm__("dey");
-  __asm__("bne %g", L1);
-  __asm__("lda ptr2+1");
-  __asm__("ldx ptr2");
-  return __AX__;
-}
-
-static void __fastcall__ beep()
+static void beep()
 {
   SV_LEFT.delay = 300;
   SV_LEFT.timer = 2;
@@ -58,14 +35,15 @@ static void __fastcall__ beep()
   SV_RIGHT.control = 0b00111111;
 }
 
-static void display_char(const unsigned char x, const unsigned char y, const unsigned char *ch, const char invert)
+static void display_char(const unsigned char x, const unsigned char y, const unsigned int *ch, const char invert)
 {
-  unsigned char i, byte;
+  unsigned char i;
+  unsigned int word;
 
   for (i = 0; i < 8; ++i) {
-    byte = ch[i];
-    if (invert) byte = ~byte;
-    SV_VRAM[y][i][x] = double_bits(byte);
+    word = ch[i];
+    if (invert) word = ~word;
+    SV_VRAM[y][i][x] = word;
   }
 }
 
@@ -80,33 +58,43 @@ static void print(char *str, int len, unsigned char x, unsigned char y, char inv
   unsigned char i;
 
   for (i = 0; i < len; i++) {
-    char character = *(str + i * sizeof(char));
-    display_char(x + i, y, font + (character - 32) * sizeof(unsigned char) * 8, invert);
+    char character = str[i];
+    display_char(x + i, y, &font[(character - 32) * 8], invert);
   }
 }
 
-void sleep(int count)
-{
-  while (count) {
-    __asm__("nop");
-    count--;
-  }
+void wait_200ms() {
+    __asm__("ldx #$C8");
+    __asm__("ms200_outer:");
+    
+    // Внутренний цикл ~4000 циклов (1мс)
+    __asm__("ldy #$00");     // 256 итераций
+    __asm__("ms200_middle:");
+    __asm__("lda #$0F");     // ~15 итераций по ~1 циклу = ~15 циклов
+    __asm__("ms200_inner:");
+    __asm__("dec a");
+    __asm__("bne ms200_inner");
+    __asm__("dey");
+    __asm__("bne ms200_middle");
+    
+    __asm__("dex");
+    __asm__("bne ms200_outer");
 }
 
-void reset()
+void wait() {
+  wait_200ms();
+}
+
+void __fastcall__ reset()
 {
-  __asm__("SEI");
-  __asm__("LDA #$00");
-  __asm__("STA $2010");
-  __asm__("STA $2014");
-  __asm__("STA $201B");
-  __asm__("STA $202A");
-  __asm__("STA $200D");
-  __asm__("LDA #$01");
-  __asm__("STA $2026");
-  __asm__("LDA #$00");
-  __asm__("STA $2026");
-  __asm__("JMP ($FFFA)");
+    __asm__("sei");
+    __asm__("cld");
+    __asm__("ldx #$FF");
+    __asm__("txs");
+    __asm__("lda #$08");
+    __asm__("sta $2026");
+    __asm__("cli");
+    __asm__("jmp ($FFFC)");
 }
 
 void print_game_item(unsigned char page, unsigned char line, unsigned char selected) 
@@ -133,65 +121,62 @@ void draw_games_list(unsigned char page, unsigned char cursor)
 
     for (i; i < end_game; i++) 
     {
-        unsigned char line = i - start_game + 1;  // Номер строки (1..19)
+        unsigned char line = i - start_game + 1;
         print_game_item(page, line, line == cursor);
     }
 }
 
 void main(void) 
 {
-    unsigned char cursor = 1;  // Текущая позиция курсора (1..19)
-    unsigned char page = 0;    // Текущая страница (0..N)
+    unsigned char cursor = 1;
+    unsigned char page = 0;
     unsigned char total_pages = (GAMECOUNT + GAMES_PER_PAGE - 1) / GAMES_PER_PAGE;
+    unsigned char lastPageGameCount = GAMECOUNT % GAMES_PER_PAGE;
     
     init();
     clear_screen();
-    draw_games_list(page, cursor);  // Первоначальная отрисовка списка
+    draw_games_list(page, cursor);
 
     while (1) 
     {
         if ((~SV_CONTROL & JOY_DOWN_MASK) && (cursor < GAMES_PER_PAGE)) 
-        {           
-            // Сдвиг курсора вниз
-            print_game_item(page, cursor, 0);  // Убираем выделение
+        {
+            if(page == total_pages - 1 && cursor == lastPageGameCount) continue;
+            print_game_item(page, cursor, 0);
             cursor++;
-            print_game_item(page, cursor, 1);  // Выделяем новую позицию
+            print_game_item(page, cursor, 1);
             beep();
             while(~SV_CONTROL & JOY_DOWN_MASK){};
         }
         else if ((~SV_CONTROL & JOY_UP_MASK) && (cursor > 1)) 
         {
-            // Сдвиг курсора вверх
-            print_game_item(page, cursor, 0);  // Убираем выделение
+            print_game_item(page, cursor, 0);
             cursor--;
-            print_game_item(page, cursor, 1);  // Выделяем новую позицию
+            print_game_item(page, cursor, 1);
             beep();
             while(~SV_CONTROL & JOY_UP_MASK){};
         }
         else if ((~SV_CONTROL & JOY_LEFT_MASK) && (page > 0)) 
         {
-            // Переход на предыдущую страницу
             page--;
-            cursor = 1;  // Сбрасываем курсор на 1-ю позицию
-            draw_games_list(page, cursor);  // Полная перерисовка
+            cursor = 1;
+            draw_games_list(page, cursor);
             beep();
             while(~SV_CONTROL & JOY_LEFT_MASK){};
         }
         else if ((~SV_CONTROL & JOY_RIGHT_MASK) && (page < total_pages - 1)) 
         {  
-            // Переход на следующую страницу
             page++;
-            cursor = 1;  // Сбрасываем курсор на 1-ю позицию
-            draw_games_list(page, cursor);  // Полная перерисовка
+            cursor = 1;
+            draw_games_list(page, cursor);
             beep();
             while(~SV_CONTROL & JOY_RIGHT_MASK){};
         }
         else if (~SV_CONTROL & JOY_START_MASK) 
         {
-            // Запуск выбранной игры
             unsigned char game_index = page * GAMES_PER_PAGE + cursor;
             SV_LCD.ypos = *(unsigned char*)(0x9000 + game_index);
-            while(~SV_CONTROL & JOY_START_MASK){};
+            wait();
             reset();
         }
     }
